9b9c6512b64209e3bbd8bef81ad4bd653728b901
Elias Farhan <e.farhan@sae.edu>
2019-09-25 11:37:38 +0200

    First commit, added Math functions max

---
+++ MultipleFiles/MathFunc.cpp
@@ -0,0 +1,15 @@
+
+#include "pch.h"
+
+int max_numbers(int* array, int size)
+{
+	int index_max_nmb = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (array[index_max_nmb] < array[i])
+		{
+			index_max_nmb = i;
+		}
+	}
+	return array[index_max_nmb];
+}
